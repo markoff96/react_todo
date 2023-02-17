c3db1cf9e0c4976b912da329a032d257f914467b
Kirill <punisher543@mail.com>
2023-02-17 18:08:34 +0300

    deleting logs

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ function App(props) {
  
   function createNewTask(event) {
     event.preventDefault()
-    console.log(text)
-    console.log(text)
-    console.log(text)
   }
 
   return <div className="App">
